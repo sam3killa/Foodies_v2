d6d44a44f14c1bfdc9ad4bce2d76370276ab2f5f
weichenux <chanwei@umich.edu>
2015-02-24 18:00:35 -0800

    initial commit

--- Foodies_v2/ViewController.swift
+++ Foodies_v2/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loadingView: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.enabled = false
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +27,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func checkPassword (){
+        if (self.passwordField.text == "000000"){
+            performSegueWithIdentifier("loginSegue", sender: self)
+        }
+        else {
+            var alertView = UIAlertView(title: "Login Failed", message: "Invalid Email/Password", delegate: self, cancelButtonTitle: "ok")
+            alertView.show()
+            self.loginButton.enabled = false
+        }
+        self.loadingView.stopAnimating()
+    }
 
+    @IBAction func didEnterEmail(sender: AnyObject) {
+        if (emailField.text.isEmpty || passwordField.text.isEmpty){
+            loginButton.enabled = false
+        }
+        else {
+            loginButton.enabled = true
+        }
+    }
+
+    @IBAction func didEnterPassword(sender: AnyObject) {
+        if (emailField.text.isEmpty || passwordField.text.isEmpty){
+            loginButton.enabled = false
+        }
+        else {
+            loginButton.enabled = true
+        }
+    }
+
+    
+
+
+    
+    @IBAction func didPressLoginButton(sender: AnyObject) {
+        loadingView.startAnimating()
+        
+        self.loginButton.selected = true
+        delay(2, {() -> () in
+            self.checkPassword()
+            self.loginButton.selected = false
+        })
+
+    }
+    
+    
 
 }
 
